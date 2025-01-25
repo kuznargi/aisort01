71dc1c79f6a201dce1560b65efc96bea41732ce3
Nargiza <kuzybahovanargiza@gmail.com>
2025-01-25 09:04:55 +0500

    full last changes

--- aisort/urls.py
+++ aisort/urls.py
@@ -1,8 +1,15 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from app.views import index,process_frame,video_feed_yolo,analysis
+from app import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('admin/', admin.site.urls),
+    # path('',index,name='index'),
+    # path('video/', test, name='test'),
+    # path('video_feed_yolo/', video_feed_yolo, name='video_feed_yolo'),
+    # path("analysis/", analysis, name="analysis"),
+        path('', views.index, name='index'),
     path('video_feed_yolo/', views.video_feed_yolo, name='video_feed_yolo'),
     path('analysis/', views.analysis, name='analysis'),
     path('process_frame/', views.process_frame, name='process_frame'),
